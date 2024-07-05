8e2680660ecca8ab20d916673e293e09368e5c3b
floy <git@floy.soy>
2024-07-05 18:54:35 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Flake for spaceos";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShell = pkgs.mkShell {
+        packages = with pkgs; [
+          lua
+          gnumake
+        ];
+      };
+    }
+  );
+}
